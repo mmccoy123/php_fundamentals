db05d249c69280f9d4befa03379d582a16f6c743
esaygo <elena.saygo@gmail.com>
2016-04-05 11:15:37 -0700

    html_table css finalised

--- html_table.php
+++ html_table.php
@@ -12,6 +12,11 @@ th, td {
   border: 1px solid #bdc6bc;
 }
 
+.black {
+  background-color: #000;
+  color: #fff;
+}
+
 </style>
 
 
@@ -26,7 +31,17 @@ $users = array(
    array('first_name' => 'KB',
          'last_name' => 'Tonel'),
    array('first_name' => 'Barak',
-         'last_name' => 'Obama')
+         'last_name' => 'Obama'),
+   array('first_name' => 'Michael',
+         'last_name' => 'Choi'),
+   array('first_name' => 'John',
+         'last_name' => 'Supsupin'),
+   array('first_name' => 'Mark',
+         'last_name' => 'Guillen'),
+   array('first_name' => 'KB',
+         'last_name' => 'Tonel'),
+   array('first_name' => 'Barak',
+        'last_name' => 'Obama')
 );
 
 ?>
@@ -42,15 +57,22 @@ $users = array(
       <th>Length of full name (without spaces)</th>
     </tr>
         <?php for($i = 0; $i < count($users); $i++) {
-          $row = $users[$i];
+          // $row = $users[$i];
+          if(($i+1) % 5 == 0) {
         ?>
-         <tr>
+
+         <tr class="black">
+         <?php } else { ?>
+             <tr>
+            <?php } ?>
+
           <th>
             <?php echo $i + 1; ?>
           </th>
+
               <?php
                   $full_name="";
-                  foreach ($row as $key => $value) {
+                  foreach ($users[$i] as $key => $value) {
               ?>
                 <td>
                   <?php  echo $value;
@@ -63,20 +85,10 @@ $users = array(
                   <?php echo strtoupper($full_name); ?>
                 </td>
                 <td>
-                  <?php echo strlen(trim($full_name, " ")); ?>
+                  <?php echo strlen(str_replace(" ","",$full_name)); ?>
                 </td>
                 </tr>
           <?php  } ?>
-
-
-    <!-- <tr>
-      <th>1</th>
-      <td>Michael</td>
-      <td>Choi</td>
-      <td>Michael Choi</td>
-      <td>MICHAEL CHOI</td>
-      <td>11</td>
-    </tr> -->;
 
   </table>
 </html>
